8076cc19e746a2765b1a2b0410f998fa81c34a6a
anettemarjaana <51910985+anettemarjaana@users.noreply.github.com>
2021-03-29 19:55:07 +0300

    Additional requirements -version

--- client.py
+++ client.py
@@ -1,40 +1,55 @@
-import sys
-from xmlrpc.client import ServerProxy
-from datetime import datetime
+# DISTRIBUTED SYSTEMS | Assignment 2 | Anette Sarivuo | 0544022
 
+import sys # for catching errors
+from xmlrpc.client import ServerProxy # for allowing clients to join the XML RCP server
+from datetime import datetime # for timestamps
+
+ #### CONNECTING CLIENTS ####    with server through a proxy:
 s = ServerProxy("http://localhost:3000")
 
 if __name__ == "__main__":
-    # BUILD A SIMPLE USER INTERFACE
     instructions = "\n1) Write a new note\n2) Append to a note\n3) Read notes\n0) Exit"
     print("\nWelcome in the Notebook App!"+instructions)
     userInput = "-1" # initialize userInput value
     
     try:
+        #### HELPFUL FUNCTIONS ####
         # Function for getting the timestamp (for the notes)
         def getTimeStamp():
             timeNow = datetime.now()
             timestampStr = timeNow.strftime("%d/%m/%Y - %H:%M:%S")
             return timestampStr
         
+        def checkIfInteger(inputValue):   
+            try:
+               inputValue = int(inputValue)
+            except ValueError:
+                print("Invalid input! Enter an integer next time.")
+                inputValue = -1
+            return inputValue
+        
         def printTopicList():
             print("\n--- Topics in the database ---")
             topicList = s.listTopics()
             print(topicList)
             topicID = input("Select topic (number): ")
-            try:
-               topicID = int(topicID)
-            except ValueError:
-                print("Invalid input! Enter an integer next time.")
-                topicID = -1
-            return topicID
+            return checkIfInteger(topicID)
+        
+        def selectAppendMethod():
+            print("\nHow do you want to modify the note?")
+            print("\n1) I want to add my own content only\n2) Let's get content from Wikipedia\n3) Wanna add a link to a Wiki article")
+            methodChoice = input("Select method (number): ")
+            return checkIfInteger(methodChoice)
+            
     
-        # LOOP THROUGH USER INPUTS: if the input is 0, client session ends
+        #### LOOP THROUGH USER INPUTS ####
+        # if the input is 0, client session ends
+        
         while (userInput != "0"):
             userInput = input("\nEnter: ")
             
             if (userInput == "1"):
-                # CREATE A NEW NOTE
+                ##### CREATE A NEW NOTE ####
                 print("\n--- Write a new note ---")
                 topic = input("Enter new topic: ")
                 text = input("Enter text: ")
@@ -48,10 +63,26 @@ if __name__ == "__main__":
                 print(instructions)
                 
             elif (userInput == "2"):
-                # APPEND TO AN EXISTING NOTE               
+                #### APPEND TO AN EXISTING NOTE  ####
+                print("\n--- Append to a note ---")             
                 topicID = printTopicList() 
-                if (topicID != -1):
-                    text = input("Enter text: ")
+                if (topicID != -1):  
+                    # LET THE USER SELECT WIKIPEDIA OR NOT
+                    methodChoice = selectAppendMethod()
+                    if (methodChoice == 1):
+                        # User adds their own text snippet
+                        text = input("Enter text: ")
+                    elif (methodChoice == 2):
+                        # Extract from Wiki
+                        searchTerm = input("Enter a search term: ")
+                        text = s.getWikiExtract(searchTerm)
+                    elif (methodChoice == 3):
+                        # Link to Wiki article
+                        searchTerm = input("Enter a search term: ")
+                        text = s.getWikiLink(searchTerm)
+                    else:
+                        print("Invalid input! Enter an integer between 1 and 3")
+                    
                     timestamp = getTimeStamp()
                     success = s.appendToNote(topicID, text, timestamp)
                     if (success == 1):
@@ -61,7 +92,7 @@ if __name__ == "__main__":
                 print(instructions)
                 
             elif (userInput == "3"):
-                # LIST TOPICS AND SELECT A NOTE TO READ
+                #### LIST TOPICS AND SELECT A NOTE TO READ ####
                 topicID = printTopicList()
                 if (topicID != -1):
                     text = s.readNote(topicID)
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-import sys
-from xmlrpc.server import SimpleXMLRPCServer
-import xml.etree.ElementTree as ET
-from datetime import datetime
+# DISTRIBUTED SYSTEMS | Assignment 2 | Anette Sarivuo | 0544022
+
+import sys # for catching errors
+from xmlrpc.server import SimpleXMLRPCServer # for building the XML RCP server
+import xml.etree.ElementTree as ET # for parsing the database (XML-file)
+import requests # for HTTP requests (Wikipedia application programming interface)
+from datetime import datetime # for timestamps
 
 # GOAL OF THE SERVER: allow clients to write, modify and read notes on an XML database
 
@@ -9,7 +12,7 @@ from datetime import datetime
 with SimpleXMLRPCServer(('localhost', 3000)) as server:
     server.register_introspection_functions()
     
-    # CONNECT TO THE DATABASE MOCK
+     #### CONNECT TO THE DATABASE MOCK  ####
     # The xml tables are represented with a tree. Iterate through it with ET.
     tree = ET.parse("db.xml")
     root = tree.getroot() # root.tag = data
@@ -17,19 +20,54 @@ with SimpleXMLRPCServer(('localhost', 3000)) as server:
     timestampStr = timeNow.strftime("%d/%M/%Y %H:%M:%S")
     print("Connected to database at " + timestampStr)
     
+     #### SET UP REQUESTS FOR WIKIPEDIA OPENSEARCH ####
+    S = requests.Session()
+    URL = "https://en.wikipedia.org/w/api.php"
     
-    # topic = root[i].attrib (in the form: {'name': 'Animal Things'} )
-    # text = root[i][0].text
-    # timestamp = root[i][1].text
-    # CREATE NECESSARY FUNCTIONS
+     #### CREATE NECESSARY FUNCTIONS  ####
     
-    
+    # Function for counting how many topics there are in the database
     def countTopics():
         counter = 0   
         for topic in root.findall("topic"):
             counter += 1
         return counter
+   
+    # Function for getting a wikipedia article link with a search term
+    def getWikiLink(searchTerm):
+        # Let the user define the search term:
+        PARAMS = {
+            "action": "opensearch",
+            "format": "json",
+            "namespace": "0",
+            "limit": "1",
+            "search": searchTerm
+        }
+        # Make the query in the Wiki API
+        dataSet = S.get(url=URL, params=PARAMS).json()
+        # get wikiPageUrl out: It's the first item in the last array of dataSet
+        wikiPageUrl = dataSet[-1][0]
+        return wikiPageUrl
+     
+    # Function for getting a wikipedia article extract with a search term
+    def getWikiExtract(searchTerm):
+        # Let the user define the title of the article (search term)
+        PARAMS = {
+            "action": "query",
+            "format": "json",
+            "prop": "extracts",
+            "exlimit": "1",
+            "exintro": True,
+            "explaintext": True,
+            "titles": searchTerm
+        }
+        # Get response R:
+        R = requests.get(url=URL, params=PARAMS).json()
+        page = next(iter(R["query"]["pages"].values()))
         
+        # Return this string:
+        return page["extract"]
+    
     # Allow client to write a new note
     def writeNewNote(topic, text, timestamp):
         success = 1
@@ -63,7 +101,7 @@ with SimpleXMLRPCServer(('localhost', 3000)) as server:
         counter = countTopics()
         if (topicID > 0 and topicID < counter+1):
             root[topicID-1][0].text += f'\n{newText}'
-            root[topicID-1][1].text += f'\n{newTimestamp}'
+            root[topicID-1][1].text = f'\n{newTimestamp}'
             tree.write("db.xml")
             success = 1
         return success
@@ -92,17 +130,20 @@ with SimpleXMLRPCServer(('localhost', 3000)) as server:
         counter = countTopics()
         
         if (topicID > -1 and topicID < counter):
-            # Find topic and get its name and text
-            noteText = f'\n--- {root[topicID].get("name")} ---\n {root[topicID][0].text}'
+            # Find topic and get its name, timestamp and text
+            noteText = f'\n--- {root[topicID].get("name")} ({root[topicID][1].text}) ---\n {root[topicID][0].text}'
         return noteText
     
-    # REGISTER FUNCTIONS
+    #### REGISTER FUNCTIONS ####
+    server.register_function(getWikiLink)
+    server.register_function(getWikiExtract)
     server.register_function(writeNewNote)
     server.register_function(appendToNote)
     server.register_function(listTopics)
     server.register_function(readNote)
+    
 
-    # SET THE SERVER TO LISTEN UNTIL INTERRUPTED
+     #### SET THE SERVER TO LISTEN ####
     try:
         server.serve_forever()
     except KeyboardInterrupt:
